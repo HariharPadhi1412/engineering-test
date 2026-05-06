24651458c33746005dde3a3fc906e0bb7c64a1d7
haripadhi08 <haripadhi08@gmail.com>
2026-05-06 08:08:16 +0530

    fix: correct random generation, age filtering, and string handling bugs

--- CodeToReview.cs
+++ CodeToReview.cs
@@ -4,23 +4,21 @@ using System.Linq;
 
 namespace Utility.Valocity.ProfileHelper
 {
-    public class People
+    public class People(string name, DateTime dob)
     {
-     private static readonly DateTimeOffset Under16 = DateTimeOffset.UtcNow.AddYears(-15);
-     public string Name { get; private set; }
-     public DateTimeOffset DOB { get; private set; }
-     public People(string name) : this(name, Under16.Date) { }
-     public People(string name, DateTime dob) {
-         Name = name;
-         DOB = dob;
-     }}
+     private static readonly DateTime Under16 = DateTime.UtcNow.AddYears(-16);
+        public string Name { get; private set; } = name;
+        public DateTime DOB { get; private set; } = dob;
+        public People(string name) : this(name, Under16.Date) { }
+    }
 
     public class BirthingUnit
     {
         /// <summary>
         /// MaxItemsToRetrieve
         /// </summary>
-        private List<People> _people;
+        private readonly List<People> _people;
+        private static readonly Random _random = new Random();
 
         public BirthingUnit()
         {
@@ -32,48 +30,52 @@ namespace Utility.Valocity.ProfileHelper
         /// </summary>
         /// <param name="j"></param>
         /// <returns>List<object></returns>
-        public List<People> GetPeople(int i)
+        public List<People> GetPeople(int count)
         {
-            for (int j = 0; j < i; j++)
+            for (int j = 0; j < count; j++)
             {
-                try
-                {
-                    // Creates a dandon Name
-                    string name = string.Empty;
-                    var random = new Random();
-                    if (random.Next(0, 1) == 0) {
-                        name = "Bob";
-                    }
-                    else {
-                        name = "Betty";
-                    }
-                    // Adds new people to the list
-                    _people.Add(new People(name, DateTime.UtcNow.Subtract(new TimeSpan(random.Next(18, 85) * 356, 0, 0, 0))));
-                }
-                catch (Exception e)
-                {
-                    // Dont think this should ever happen
-                    throw new Exception("Something failed in user creation");
-                }
+               try
+               {
+                 string name = _random.Next(0, 2) == 0 ? "Bob" : "Betty";
+ 
+                 var dob = DateTime.UtcNow.Subtract(
+                     TimeSpan.FromDays(_random.Next(18, 85) * 365)
+                 );
+ 
+                 _people.Add(new People(name, dob));
+               }
+               catch (Exception ex)
+               {
+                    throw new Exception("Something failed in user creation",ex);
+               }
             }
+
             return _people;
         }
 
         private IEnumerable<People> GetBobs(bool olderThan30)
         {
-            return olderThan30 ? _people.Where(x => x.Name == "Bob" && x.DOB >= DateTime.Now.Subtract(new TimeSpan(30 * 356, 0, 0, 0))) : _people.Where(x => x.Name == "Bob");
+            if (olderThan30)
+            {
+                var cutoff = DateTime.UtcNow.AddYears(-30);
+                return _people.Where(x => x.Name == "Bob" && x.DOB <= cutoff);
+            }
+
+            return _people.Where(x => x.Name == "Bob");
         }
 
         public string GetMarried(People p, string lastName)
         {
-            if (lastName.Contains("test"))
-                return p.Name;
-            if ((p.Name.Length + lastName).Length > 255)
-            {
-                (p.Name + " " + lastName).Substring(0, 255);
-            }
+            if (p == null) throw new ArgumentNullException(nameof(p));
 
-            return p.Name + " " + lastName;
+            if (string.IsNullOrEmpty(lastName) || lastName.Contains("test"))
+                return p.Name;
+
+            var fullName = $"{p.Name} {lastName}";
+
+            return fullName.Length > 255
+                ? fullName.Substring(0, 255)
+                : fullName;
         }
     }
 }
